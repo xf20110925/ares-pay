7138e4c9fd78291c0814dcc8f157494539e0562d
wangguanhua <guanhua.wang@pintuibao.cn>
2016-11-15 16:59:18 +0800

    修改充值订单查询条件，只显示已支付和审核中的。

--- ares-pay-provider/src/main/java/com/ptb/pay/api/impl/RechargeOrderApiImpl.java
+++ ares-pay-provider/src/main/java/com/ptb/pay/api/impl/RechargeOrderApiImpl.java
@@ -1,5 +1,6 @@
 package com.ptb.pay.api.impl;
 
+import com.ptb.common.enums.RechargeOrderStatusEnum;
 import com.ptb.common.errorcode.CommonErrorCode;
 import com.ptb.common.vo.ResponseVo;
 import com.ptb.pay.api.IRechargeOrderApi;
@@ -50,6 +51,10 @@ public class RechargeOrderApiImpl implements IRechargeOrderApi {
 
         RechargeOrderExample example = new RechargeOrderExample();
         example.createCriteria().andUserIdEqualTo(userId);
+        List<Integer> status = new ArrayList<Integer>();
+        status.add(RechargeOrderStatusEnum.paid.getRechargeOrderStatus());
+        status.add(RechargeOrderStatusEnum.review.getRechargeOrderStatus());
+        example.createCriteria().andStatusIn(status);
         example.setOrderByClause("create_time desc");
         List<RechargeOrder> orders = rechargeOrderMapper.selectByExample(example);
         List<RechargeOrderVO> returnData = new ArrayList<RechargeOrderVO>();
