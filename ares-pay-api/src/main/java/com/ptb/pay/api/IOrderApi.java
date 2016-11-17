8bb1d1bdcc0be3e503be848888a91b0a7d2b25bf
jianpeng <jianpeng.liu@pintuibao.cn>
2016-11-17 10:17:51 +0800

    增加订单api

---
+++ ares-pay-api/src/main/java/com/ptb/pay/api/IOrderApi.java
@@ -0,0 +1,7 @@
+package com.ptb.pay.api;
+
+/**
+ * Created by Administrator on 2016/11/16 0016.
+ */
+public interface IOrderApi {
+}
---
+++ ares-pay-provider/src/main/java/com/ptb/pay/api/impl/OrderApiImpl.java
@@ -0,0 +1,9 @@
+package com.ptb.pay.api.impl;
+
+import com.ptb.pay.api.IOrderApi;
+
+/**
+ * Created by Administrator on 2016/11/16 0016.
+ */
+public class OrderApiImpl implements IOrderApi {
+}
