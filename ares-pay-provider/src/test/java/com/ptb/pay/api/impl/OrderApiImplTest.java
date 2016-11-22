1f333896019612202d52e5f6fe050645baab8e7e
zhangpeng <peng.zhang@pintuibao.cn>
2016-11-22 20:19:26 +0800

    添加提交订单

--- ares-pay-provider/src/test/java/com/ptb/pay/api/impl/OrderApiImplTest.java
+++ ares-pay-provider/src/test/java/com/ptb/pay/api/impl/OrderApiImplTest.java
@@ -78,7 +78,7 @@ public class OrderApiImplTest extends BaseTest {
     @Test
     public void cancelOrderTest(){
         try {
-            ResponseVo responseVo = orderApi.cancelOrder(777l, 14);
+            ResponseVo responseVo = orderApi.cancelOrder(777l, 16);
             System.out.println(responseVo.getMessage());
         }catch (Exception e){
 
