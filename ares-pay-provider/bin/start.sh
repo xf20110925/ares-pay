bbf23035a16be4f4b155bbd5f42f3d9a459d51c4
wangguanhua <guanhua.wang@pintuibao.cn>
2017-01-05 15:42:05 +0800

    修改启动参数，GC调优

--- ares-pay-provider/bin/start.sh
+++ ares-pay-provider/bin/start.sh
@@ -8,4 +8,4 @@ libjar=`ls libs | grep .jar | awk '{jar=jar"'"libs/"'"$1":"} END {print jar}'`
 echo $jars
 
 echo $CONFIG_FILE_HOME:$jars:$libjar
-java -cp $CONFIG_FILE_HOME:$libjar:$jars  com.ptb.pay.Application  $* >> logs/run.log 2>&1
+java -cp $CONFIG_FILE_HOME:$libjar:$jars -server -Xms6g -Xmx6g -XX:PermSize=128m -XX:MaxPermSize=128m -Xss256k  -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:MaxGCPauseMillis=10 -XX:GCPauseIntervalMillis=200 -XX:+DisableExplicitGC com.ptb.pay.Application  $* >> logs/run.log 2>&1
